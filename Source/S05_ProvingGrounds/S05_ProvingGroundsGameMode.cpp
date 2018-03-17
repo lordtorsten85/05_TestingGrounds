bfacc637ce776e3ce3e215116e95151bcbc252a0
Trevor Nelson <lordtorsten85@gmail.com>
2018-03-17 11:59:39 -0600

    Cleaned up bad redierctors and fixed bugs from folder structure change.

--- Source/S05_ProvingGrounds/S05_ProvingGroundsGameMode.cpp
+++ Source/S05_ProvingGrounds/S05_ProvingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AS05_ProvingGroundsGameMode::AS05_ProvingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavoir/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavoir/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
